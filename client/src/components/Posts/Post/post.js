9633158b009ba468f37942a1a9390959b2170592
ante <ante.kotarac@gmail.com>
2022-05-21 22:57:55 +0200

    styling of post card

--- client/src/components/Posts/Post/post.js
+++ client/src/components/Posts/Post/post.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
+import { Button, Card, CardActions, CardMedia, CardContent, Typography } from '@mui/material';
+import { Delete, MoreHoriz, ThumbUpAlt } from '@mui/icons-material';
+
 import './post.css';
 
-export default function Post(props) {
+export default function Post({ post }) {
     return (
-        <div>Post</div>
+        <Card className='card'>
+            <CardMedia className='media' image={post.selectedFile} title={post.title} />
+            <div className='overlay'>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+            <div className='overlay2'>
+                <Button style={{ color: 'white' }} size="small" onClick={() => { }}>
+                    <MoreHoriz fontSize='default' />
+                </Button>
+            </div>
+            <div className='details'>
+                <Typography varian="body2" color="textSecondary">{post.tags.map(tag => `#${tag} `)}</Typography>
+            </div>
+            <CardContent>
+                <Typography className="title" variant="h5" gutterBottom>{post.message}</Typography>
+            </CardContent>
+            <CardActions className='cardActions'>
+                <Button size="small" onClick={() => { }} color="primary">
+                    <ThumbUpAlt fontSize='small' />
+                    Like
+                    {post.likeCount}
+                </Button>
+                <Button size="small" onClick={() => { }} color="primary">
+                    <Delete fontSize='small' />
+                    Delete
+                </Button>
+
+            </CardActions>
+        </Card>
     )
 }
--- client/src/components/Posts/posts.js
+++ client/src/components/Posts/posts.js
@@ -1,22 +1,17 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@mui/material';
 import Post from './Post/post';
 import './posts.css';
 
-const renderPosts = (posts) => {
-    if (posts.length) {
-        return posts.map((post, index) => <Post key={index} post={post} />)
-    }
-}
-
 export default function Posts() {
     const posts = useSelector((state) => state.posts)
-    console.log(posts);
-    console.log(typeof posts);
     return (
-        <div>
-            <h1>Posts</h1>
-            {renderPosts(posts)}
-        </div>
+        !posts.length ? <CircularProgress />
+            : <Grid className="mainContainer" container alignItems="stretch" spacing={3}>
+                {
+                    posts.map(post => <Grid item key={post._id} xs={12}><Post post={post} /></Grid>)
+                }
+            </Grid>
     )
 }
