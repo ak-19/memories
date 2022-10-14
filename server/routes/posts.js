bb9c1b28140661ac32cb252bad0f6c451c7daf55
ante <ante.kotarac@gmail.com>
2022-10-14 18:32:02 +0200

    auth middlewar

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { getPosts, createPosts, deletePosts, likePosts, updatePosts } from '../controllers/posts.js';
 
+import auth from '../middleware/auth.js';
+
 const router = express.Router();
 router.get('/', getPosts)
-router.post('/', createPosts)
-router.patch('/:id', updatePosts)
-router.delete('/:id', deletePosts)
-router.patch('/:id/likepost', likePosts)
+router.post('/', auth, createPosts)
+router.patch('/:id', auth, updatePosts)
+router.delete('/:id', auth, deletePosts)
+router.patch('/:id/likepost', auth, likePosts)
 
 export default router;
