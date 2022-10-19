09ff887781fcedb187d0a77076c71c344c6514fb
ante <ante.kotarac@gmail.com>
2022-10-19 23:56:22 +0200

    client hidding like/delete/edit

--- client/src/components/Posts/Post/post.js
+++ client/src/components/Posts/Post/post.js
@@ -4,10 +4,37 @@ import { Button, Card, CardActions, CardMedia, CardContent, Typography } from '@
 import { Delete, MoreHoriz, ThumbUpAlt } from '@mui/icons-material';
 import { useDispatch } from 'react-redux';
 import { likePost, deletePost } from '../../../actions/posts'
+
 import './post.css';
+
+const likesPrintOut = (post) => {
+    if (post.likes.length === 1) return `${post.likes.length} like`
+    return `${post.likes.length} likes`
+}
+
+
 
 export default function Post({ post, setCurrentId }) {
     const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem('profile'));
+
+    const renderDelete = () => {
+        if (user?.result?._id === post.creator)
+            return (<Button size="small" onClick={() => { dispatch(deletePost(post._id)) }} color="primary">
+                <Delete fontSize='small' />
+                Delete
+            </Button>)
+        return ''
+    }
+    const renderEdit = () => {
+        if (user?.result?._id === post.creator)
+            return (<div className='overlay2'>
+                <Button style={{ color: 'white' }} size="small" onClick={() => { setCurrentId(post._id) }}>
+                    <MoreHoriz fontSize='default' />
+                </Button>
+            </div>)
+        return ''
+    }
     return (
         <Card className='card'>
             <CardMedia className='media' image={post.selectedFile} title={post.title} />
@@ -15,11 +42,7 @@ export default function Post({ post, setCurrentId }) {
                 <Typography variant="h6">{post.name}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
-            <div className='overlay2'>
-                <Button style={{ color: 'white' }} size="small" onClick={() => { setCurrentId(post._id) }}>
-                    <MoreHoriz fontSize='default' />
-                </Button>
-            </div>
+            {renderEdit()}
             <div className='details'>
                 <Typography varian="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
             </div>
@@ -30,14 +53,9 @@ export default function Post({ post, setCurrentId }) {
             <CardActions className='cardActions'>
                 <Button size="small" onClick={() => { dispatch(likePost(post._id)) }} color="primary">
                     <ThumbUpAlt fontSize='small' />
-                    &nbsp;Like &nbsp;
-                    {post.likeCount}
+                    {likesPrintOut(post)}
                 </Button>
-                <Button size="small" onClick={() => { dispatch(deletePost(post._id)) }} color="primary">
-                    <Delete fontSize='small' />
-                    Delete
-                </Button>
-
+                {renderDelete()}
             </CardActions>
         </Card>
     )
