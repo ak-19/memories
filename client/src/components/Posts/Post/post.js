14b3ff297420954a87311bd5e832eac3c31cec25
ante <ante.kotarac@gmail.com>
2022-11-01 21:41:00 +0100

    Jump to details

--- client/src/components/Posts/Post/post.js
+++ client/src/components/Posts/Post/post.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import moment from 'moment';
-import { Button, Card, CardActions, CardMedia, CardContent, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { Button, ButtonBase, Card, CardActions, CardMedia, CardContent, Typography, Link } from '@mui/material';
 import { Delete, MoreHoriz, ThumbUpAlt } from '@mui/icons-material';
 import { useDispatch } from 'react-redux';
 import { likePost, deletePost } from '../../../actions/posts'
@@ -17,7 +18,7 @@ const likesPrintOut = (post) => {
 export default function Post({ post, setCurrentId }) {
     const dispatch = useDispatch();
     const user = JSON.parse(localStorage.getItem('profile'));
-
+    const history = useNavigate();
     const renderDelete = () => {
         if (user?.result?._id === post.creator)
             return (<Button size="small" onClick={() => { dispatch(deletePost(post._id)) }} color="primary">
@@ -35,21 +36,35 @@ export default function Post({ post, setCurrentId }) {
             </div>)
         return ''
     }
+
+    const openPost = () => {
+        history(`/posts/${post._id}`)
+    }
+
     return (
         <Card className='card'>
-            <CardMedia className='media' image={post.selectedFile} title={post.title} />
-            <div className='overlay'>
-                <Typography variant="h6">{post.name}</Typography>
-                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
-            </div>
-            {renderEdit()}
-            <div className='details'>
-                <Typography varian="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
-            </div>
-            <Typography className="title" color="textSecondary" variant="h5">{post.title}   </Typography>
-            <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p" >{post.message}</Typography>
-            </CardContent>
+            <Link
+                component="button"
+                variant="string"
+                onClick={openPost}
+                underline="none"
+                sx={{ textAlign: 'left' }}
+            >
+
+                <CardMedia className='media' image={post.selectedFile} title={post.title} />
+                <div className='overlay'>
+                    <Typography variant="h6">{post.name}</Typography>
+                    <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+                </div>
+                {renderEdit()}
+                <div className='details'>
+                    <Typography varian="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
+                </div>
+                <Typography className="title" color="textSecondary" variant="h5">{post.title}   </Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p" >{post.message}</Typography>
+                </CardContent>
+            </Link>
             <CardActions className='cardActions'>
                 <Button size="small" onClick={() => { dispatch(likePost(post._id)) }} color="primary">
                     <ThumbUpAlt fontSize='small' />
